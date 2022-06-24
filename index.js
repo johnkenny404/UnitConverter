d8d295c16bac6a51d9b8ca9ff384130bf7396fe8
johnkenny404 <johnkenny404@gmail.com>
2022-06-24 14:56:59 +0100

    round off

--- index.js
+++ index.js
@@ -11,14 +11,16 @@ const Kilo = document.querySelector(".display-mass");
 convertBtn.addEventListener("click", function () {
   let BaseValue = inputNum.value;
 
-  lengthMeter.textContent = `${BaseValue} Meters = ${
+  lengthMeter.textContent = `${BaseValue} Meters = ${(
     BaseValue * 3.281
-  }Feets | ${BaseValue} Feets = ${BaseValue / 3.281}Meters`;
+  ).toFixed(4)}Feets | ${BaseValue} Feets = ${(BaseValue / 3.281).toFixed(
+    4
+  )}Meters`;
 
-  Volume.textContent = `${BaseValue} Volume = ${
-    BaseValue * 0.264
-  }Gallons | ${BaseValue} Gallons = ${BaseValue / 0.264}Volume`;
-  Kilo.textContent = `${BaseValue} Kilogram = ${
-    BaseValue * 2.205
-  }Pounds | ${BaseValue} Pounds = ${BaseValue / 2.204}Kilograms`;
+  Volume.textContent = `${BaseValue} Volume = ${(BaseValue * 0.264).toFixed(
+    4
+  )}Gallons | ${BaseValue} Gallons = ${(BaseValue / 0.264).toFixed(4)}Volume`;
+  Kilo.textContent = `${BaseValue} Kilogram = ${(BaseValue * 2.205).toFixed(
+    4
+  )}Pounds | ${BaseValue} Pounds = ${(BaseValue / 2.204).toFixed(4)}Kilograms`;
 });
